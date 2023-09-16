2489ced31c39953ad07e81b39273d209c63ed804
Alina <AZharkevich@sportmasterlab.net>
2023-09-16 19:32:08 +0500

    add 6.2 homework

---
+++ src/SixthPointTwoHw.java
@@ -0,0 +1,48 @@
+public class SixthPointTwoHw {
+
+    public static void main(String[] args) {
+        first_task();
+        second_task();
+        third_task();
+    }
+
+    public static void first_task() {
+        String text = "0  1  2  3  4  5  6  7  8  9\n" +
+                "0  1  2  3  4  5  6  7  8\n" +
+                "0  1  2  3  4  5  6  7\n" +
+                "0  1  2  3  4  5  6\n" +
+                "0  1  2  3  4  5\n" +
+                "0  1  2  3  4\n" +
+                "0  1  2  3\n" +
+                "0  1  2\n" +
+                "0  1\n" +
+                "0\n" ;
+        System.out.println(text);
+    }
+    public static void second_task() {
+        String text = "0  1  2  3  4  5  6  7  8  9\n" +
+                "   0  1  2  3  4  5  6  7  8\n" +
+                "      0  1  2  3  4  5  6  7\n" +
+                "         0  1  2  3  4  5  6\n" +
+                "            0  1  2  3  4  5\n" +
+                "               0  1  2  3  4\n" +
+                "                  0  1  2  3\n" +
+                "                     0  1  2\n" +
+                "                        0  1\n" +
+                "                           0\n" ;
+        System.out.println(text);
+    }
+    public static void third_task() {
+        String text = "9 8 7 6 5 4 3 2 1 0 1 2 3 4 5 6 7 8 9\n" +
+                "  8 7 6 5 4 3 2 1 0 1 2 3 4 5 6 7 8\n" +
+                "    7 6 5 4 3 2 1 0 1 2 3 4 5 6 7\n" +
+                "      6 5 4 3 2 1 0 1 2 3 4 5 6\n" +
+                "        5 4 3 2 1 0 1 2 3 4 5\n" +
+                "          4 3 2 1 0 1 2 3 4\n" +
+                "            3 2 1 0 1 2 3\n" +
+                "              2 1 0 1 2\n" +
+                "                1 0 1\n" +
+                "                  0\n" ;
+        System.out.println(text);
+    }
+}
