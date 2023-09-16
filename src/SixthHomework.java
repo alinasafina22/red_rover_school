944be42eb41d0e00bd53ca436306a5748f66d8b2
Alina <AZharkevich@sportmasterlab.net>
2023-09-16 19:24:57 +0500

    add six homework

---
+++ src/SixthHomework.java
@@ -0,0 +1,72 @@
+public class SixthHomework {
+    public static void main(String[] args) {
+        System.out.println(first_task());
+        System.out.println(second_task());
+        System.out.println(third_task());
+        System.out.println(fourth_task());
+    }
+//    Задача №1
+//    Дан массив:
+//    int[][] array = {{1, 2, 3, 4, 5}, {6, 7, 8, 9}, {-1, -2, -3, -4}, {-5, -6}};
+//    необходимо вывести сумму элементов массива.
+
+    public static int first_task() {
+        int[][] array = {{1, 2, 3, 4, 5}, {6, 7, 8, 9}, {-1, -2, -3, -4}, {-5, -6}};
+        int sum = 0;
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array[i].length; j++) {
+                sum += array[i][j];
+            }
+        }
+        return sum;
+    }
+//    Задача №2
+//    Дан массив:
+//    int[][] array = {{1, 2, 3, 4, 5}, {6, 7, 8, 9}, {-1, -2, -3, -4}, {-5, -6}};
+//    необходимо вывести максимальное значение массива.
+
+    public static int second_task() {
+        int[][] array = {{1, 2, 3, 4, 5}, {6, 7, 8, 9}, {-1, -2, -3, -4}, {-5, -6}};
+        int max = array[0][0];
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array[i].length; j++) {
+                if (array[i][j] > max) {
+                    max = array[i][j];
+                }
+            }
+        }
+        return max;
+    }
+
+    //    Задача №3
+//    Дан массив:
+//    int[][] array = {{1, 2, 3, 4, 5}, {6, 7, 8, 9}, {-1, -2, -3, -4}, {-5, -6}};
+//    необходимо вывести минимальное значение массива.
+    public static int third_task() {
+        int[][] array = {{1, 2, 3, 4, 5}, {6, 7, 8, 9}, {-1, -2, -3, -4}, {-5, -6}};
+        int min = array[0][0];
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array[i].length; j++) {
+                if (array[i][j] < min) {
+                    min = array[i][j];
+                }
+            }
+        }
+        return min;
+    }
+
+    //    Задача №3
+//    Дан массив:
+//    int[][] array = {{1, 2, 3, 4, 5}, {6, 7, 8, 9}, {-1, -2, -3, -4}, {-5, -6}};
+//    необходимо вывести количество элементов в массиве.
+    public static int fourth_task() {
+        int[][] array = {{1, 2, 3, 4, 5}, {6, 7, 8, 9}, {-1, -2, -3, -4}, {-5, -6}};
+        int count = 0;
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array[i].length; j++) {
+                count++;
+            }
+        }
+        return count;
+    }
+}
