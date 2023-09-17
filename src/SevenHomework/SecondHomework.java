d2cb71258a5cf31538931f4c587ff22790c38c89
Alina <AZharkevich@sportmasterlab.net>
2023-09-17 09:09:53 +0500

    add 7 homework 3 task

--- src/SevenHomework/SecondHomework.java
+++ src/SevenHomework/SecondHomework.java
@@ -10,10 +10,16 @@ public class SecondHomework {
     public static void main(String[] args) {
         Employee kim = new Employee();
         Employee kendall = new Employee();
+        Employee[] array = new Employee[2];
         kim.name = "Kim";
         kendall.name = "Kendall";
+        kim.salary = 100;
+        kendall.salary = 350;
+        array[0] = kim;
+        array[1] = kendall;
         System.out.println(kendall.isSameName(kim));
-
+        Salary salary = new Salary();
+        System.out.println(salary.getSum(array));
     }
 }
 
@@ -28,5 +34,14 @@ class Employee {
         return employee.name == name;
     }
 }
+class Salary {
+    int getSum(Employee[] employeeArray){
+        int sum = 0;
+        for (Employee employee : employeeArray) {
+            sum += employee.salary;
+        }
+        return sum;
+    }
 
+}
 
